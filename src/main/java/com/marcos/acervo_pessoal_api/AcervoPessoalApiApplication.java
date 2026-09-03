d6ee226748fdd4dc4eba3e087eb93ce117f3d3fb
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-09-03 20:48:08 -0300

    chore :setup inicial do projeto

---
+++ src/main/java/com/marcos/acervo_pessoal_api/AcervoPessoalApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.acervo_pessoal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervoPessoalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervoPessoalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/acervo_pessoal_api/AcervoPessoalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.acervo_pessoal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoPessoalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
