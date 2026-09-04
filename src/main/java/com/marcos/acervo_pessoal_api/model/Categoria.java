be24fd289770c3db0740af166b8ec1a09f8f1999
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-09-03 21:42:17 -0300

    feat :adiciona a Classe Livro, sendo a entidade porincipal, e classe enum Categoria, para definar as opções do campo categoria da entidade Livro

---
+++ src/main/java/com/marcos/acervo_pessoal_api/model/Categoria.java
@@ -0,0 +1,5 @@
+package com.marcos.acervo_pessoal_api.model;
+
+public enum Categoria {
+    HQ, FANTASIA, MANGA, AUTOAJUDA, DIDATICO, ROMANCE, SUSPENSE, TERROR
+}
---
+++ src/main/java/com/marcos/acervo_pessoal_api/model/Livro.java
@@ -0,0 +1,117 @@
+package com.marcos.acervo_pessoal_api.model;
+
+import jakarta.persistence.*;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "livro")
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String titulo;
+    @Column(nullable = false)
+    private String autor;
+    @Column(nullable = false)
+    private String editora;
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+    @Column(nullable = false, unique = true)
+    private String isbn;
+    @Column(nullable = false)
+    private boolean disponibilidade;
+    @Column
+    private boolean lido;
+    @Column(nullable = false)
+    private LocalDate dataPublicacao;
+
+
+    public Livro() {
+    }
+
+    public Livro(Long id, String titulo, String autor, String editora, String isbn, boolean disponibilidade, boolean lido, LocalDate dataPublicacao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.isbn = isbn;
+        this.disponibilidade = disponibilidade;
+        this.lido = lido;
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isDisponibilidade() {
+        return disponibilidade;
+    }
+
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+
+    public boolean isLido() {
+        return lido;
+    }
+
+    public void setLido(boolean lido) {
+        this.lido = lido;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+}
